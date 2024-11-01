29517b4135da607e184819863486d35993739fc2
Алексей <77805051+Morgeous29@users.noreply.github.com>
2024-11-01 19:47:40 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,21 +1,14 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
-positive_numbers = []
 index = 0
-
 while index < len(my_list):
-    if my_list[index] > 0:
-        positive_numbers.append(my_list[index])
-        index += 1
-    elif my_list[index] == 0:
-        index += 1
-    else:
-        break
-
-index = 0
-
-while index < len(positive_numbers):
-    print(positive_numbers[index])
+    x = my_list[index]
     index += 1
-    continue
-    break
+    if x == 0:
+        continue
+    elif x < 0:
+        break
+    elif index == len(my_list):
+        print(x)
+    else:
+        print(x)
